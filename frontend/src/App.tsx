f60ec324cc9a84cd65a58acb58f193b378075507
Aryan Sharma <122178949+aryansh05@users.noreply.github.com>
2026-06-29 22:54:37 +0530

    chore(frontend): initialize react with vite
    - Add all required packages for the frontend
    
    Signed-off-by: Aryan Sharma <122178949+aryansh05@users.noreply.github.com>

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="flex justify-center text-4xl font-bold">
+          Welcome to Ticketing
+      </h1>
+    </>
+  )
+}
+
+export default App
